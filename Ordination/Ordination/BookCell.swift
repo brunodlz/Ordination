b04fd1e4dc27620f4728850b1024c74577bbf224
Bruno da Luz <brunolabx@gmail.com>
2016-08-29 03:19:44 -0300

    viewcontroller removed and added BookViewController created files for future implementation

---
+++ Ordination/Ordination/BookCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookCell.swift
+//  Ordination
+//
+//  Created by Bruno da Luz on 8/29/16.
+//  Copyright © 2016 Bruno da Luz. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
