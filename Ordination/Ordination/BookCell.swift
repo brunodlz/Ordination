42218a9f2f93a3e5fa8ee741575864288bbff99b
Bruno da Luz <brunolabx@gmail.com>
2016-11-16 22:32:42 -0200

    removed comment blocks

--- Ordination/Ordination/BookCell.swift
+++ Ordination/Ordination/BookCell.swift
@@ -1,11 +1,3 @@
-//
-//  BookCell.swift
-//  Ordination
-//
-//  Created by Bruno da Luz on 8/29/16.
-//  Copyright © 2016 Bruno da Luz. All rights reserved.
-//
-
 import UIKit
 
 class BookCell: UITableViewCell {
--- Ordination/Ordination/ServiceOrder.swift
+++ Ordination/Ordination/ServiceOrder.swift
@@ -1,11 +1,3 @@
-//
-//  ServiceOrder.swift
-//  Ordination
-//
-//  Created by Bruno da Luz on 8/29/16.
-//  Copyright © 2016 Bruno da Luz. All rights reserved.
-//
-
 import Foundation
 
 class ServiceOrder {
