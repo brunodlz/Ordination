83aa535bed7c5aff3ae245cdfbe8e626bdd79aa6
Bruno da Luz <brunodlz@users.noreply.github.com>
2017-02-04 23:21:40 -0200

    Update ServiceOrder.swift

--- Ordination/Ordination/ServiceOrder.swift
+++ Ordination/Ordination/ServiceOrder.swift
@@ -29,55 +29,37 @@ class ServiceOrder {
     //# MARK: - TITLE --> ASC
 
     func getTitleASC(list listOfSorting: [Book]) -> [Book] {
-        let list_Title_ASC = listOfSorting.sorted {
-            $0.title < $1.title
-        }
-        return list_Title_ASC
+        return listOfSorting.sorted { $0.title < $1.title }
     }
 
     //# MARK: - TITLE --> DESC
 
     func getTitleDESC(list listOfSorting: [Book]) -> [Book] {
-        let list_Title_DESC = listOfSorting.sorted {
-            $0.title > $1.title
-        }
-        return list_Title_DESC
+        return listOfSorting.sorted { $0.title > $1.title }
     }
 
     //# MARK: - AUTHOR --> ASC
 
     func getAuthorASC(list listOfSorting: [Book]) -> [Book] {
-        let list_Author_ASC = listOfSorting.sorted {
-            $0.author < $1.author
-        }
-        return list_Author_ASC
+        return listOfSorting.sorted { $0.author < $1.author }
     }
 
     //# MARK: - AUTHOR --> DESC
 
     func getAuthorDESC(list listOfSorting: [Book]) -> [Book] {
-        let list_Author_DESC = listOfSorting.sorted {
-            $0.author > $1.author
-        }
-        return list_Author_DESC
+        return listOfSorting.sorted { $0.author > $1.author }
     }
 
     //# MARK: - EDITION YEAR --> ASC
 
     func getEditionYearASC(list listOfSorting: [Book]) -> [Book] {
-        let list_EditionYear_ASC = listOfSorting.sorted {
-            $0.editionYear < $1.editionYear
-        }
-        return list_EditionYear_ASC
+        return listOfSorting.sorted { $0.editionYear < $1.editionYear }
     }
 
     //# MARK: - EDITION YEAR --> DESC
 
     func getEditionYearDESC(list listOfSorting: [Book]) -> [Book] {
-        let list_EditionYear_DESC = listOfSorting.sorted {
-            $0.editionYear > $1.editionYear
-        }
-        return list_EditionYear_DESC
+        return listOfSorting.sorted { $0.editionYear > $1.editionYear }
     }
 
 }
